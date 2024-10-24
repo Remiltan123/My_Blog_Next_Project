eded6e2d0fb99b7147ee13970d191dd3312c852c
remiltan123 <regiremiltan2002@gmail.com>
2024-10-24 11:51:38 -0700

    Hero page added 24/10

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,28 @@
 import Image from "next/image";
+import Hero from '../public/hero.png'
+import styles from './page.module.css'
 
 export default function Home() {
   return (
-       <>
-         <div className="container">
-           <h1>Home</h1>
-         </div>
-       </>
+    <>
+      <div className={styles.container}>
+        <div className={styles.item }>
+          <h1 className={styles.title}>Better design for your digital products.</h1>
+
+          <p className={styles.desc}>Turning your Idea into Reality. We bring together the teams from the
+            global tech industry.</p>
+
+          <button className={styles.button }>See our Work</button>
+        
+        </div>
+
+        <div className={styles.item }>
+          <Image src={Hero} alt="Hero Image" className={styles.img}/>
+        </div>
+
+      </div>
+
+
+    </>
   );
 }
--- component/Footer/Footer.jsx
+++ component/Footer/Footer.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import styles from "./footer.module.css";
 
 export const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>©2023 Lamamia. All rights reserved.</div>
+      <div className={styles.social}>
+        <img src="/1.png" width={20} height={20} className={styles.icon} />
+        <Image src="/2.png" width={20} height={20} className={styles.icon} alt="Icon 2" />
+        <Image src="/3.png" width={20} height={20} className={styles.icon} alt="Icon 3" />
+        <Image src="/4.png" width={20} height={20} className={styles.icon} alt="Icon 4" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
